2693a4e76ffeb1ddf3eed3505caf040728f58f55
CaptainVoronin@gmail.com <CaptainVoronin@gmail.com>
2018-09-08 23:00:32 +0300

    ...

--- src/ru/nextbi/generation/atomic/DateTimeGenerator.java
+++ src/ru/nextbi/generation/atomic/DateTimeGenerator.java
@@ -1,4 +1,46 @@
 package ru.nextbi.generation.atomic;
 
-public class DateTimeGenerator{
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class DateTimeGenerator implements IGenerator{
+
+    Date start;
+    Date end;
+    SimpleDateFormat formatter;
+
+    @Override
+    public void setParamString(String rawParams) throws Exception
+    {
+        Map<String, String> params = GeneratorUtils.parseParams( rawParams );
+        end = Calendar.getInstance().getTime();
+        String format = params.getOrDefault( "format", "dd.MM.yyyy HH:mm" );
+        formatter = new SimpleDateFormat( format );
+        String buff = params.get( "start" );
+        if( buff == null )
+        {
+            System.out.println( "Start value is omitted. Set to 01.01.1970 00:00" );
+            buff = "01.01.1970 00:00";
+        }
+        start = formatter.parse( buff );
+        if( ( buff = params.get( "end" )) != null )
+            end = formatter.parse( buff );
+        else
+            System.out.println( "Start value is omitted. Set to " + formatter.format( end )  );
+
+        if( start.getTime() >= end.getTime() )
+            throw new Exception( "Incorrect range: start is greate or equal than end" );
+    }
+
+    @Override
+    public String getValue()
+    {
+        Date dt = new Date();
+        long res = ThreadLocalRandom.current().nextLong( start.getTime(), end.getTime() );
+        dt.setTime( res );
+        return formatter.format( dt );
+    }
 }
