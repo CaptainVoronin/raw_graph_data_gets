9ef3313e4713b7b17725bdb307400f2008e4ad6a
CaptainVoronin <CaptainVoronin@gmail.com>
2018-09-11 00:40:50 +0300

    Теперь понимает параметры: формат, in-format, out-format

--- src/ru/nextbi/generation/atomic/DateTimeGenerator.java
+++ src/ru/nextbi/generation/atomic/DateTimeGenerator.java
@@ -10,25 +10,39 @@ public class DateTimeGenerator  extends AbstractGenerator{
 
     Date start;
     Date end;
-    SimpleDateFormat formatter;
+    SimpleDateFormat inFormatter, outFormatter;
 
     @Override
     public void setParams(Map<String, String> config, Map<String, String> params) throws Exception
     {
+        String inFormat= "dd.MM.yyyy HH:mm";
+        String outFormat = inFormat;
+
         end = Calendar.getInstance().getTime();
-        String format = params.getOrDefault( "format", "dd.MM.yyyy HH:mm" );
-        formatter = new SimpleDateFormat( format );
+
+        if( params.containsKey( "format" ) )
+            inFormat = outFormat = params.get("format");
+
+        if( params.containsKey( "in-format" ) )
+            inFormat = params.get("in-format");
+
+        if( params.containsKey( "out-format" ) )
+            outFormat = params.get("out-format");
+
+        inFormatter = new SimpleDateFormat( inFormat );
+        outFormatter = new SimpleDateFormat( outFormat );
+
         String buff = params.get( "start" );
         if( buff == null )
         {
             System.out.println( "Start value is omitted. Set to 01.01.1970 00:00" );
             buff = "01.01.1970 00:00";
         }
-        start = formatter.parse( buff );
+        start = inFormatter.parse( buff );
         if( ( buff = params.get( "end" )) != null )
-            end = formatter.parse( buff );
+            end = inFormatter.parse( buff );
         else
-            System.out.println( "Start value is omitted. Set to " + formatter.format( end )  );
+            System.out.println( "Start value is omitted. Set to " + inFormatter.format( end )  );
 
         if( start.getTime() >= end.getTime() )
             throw new Exception( "Incorrect range: start is greate or equal than end" );
@@ -40,7 +54,6 @@ public class DateTimeGenerator  extends AbstractGenerator{
         Date dt = new Date();
         long res = ThreadLocalRandom.current().nextLong( start.getTime(), end.getTime() );
         dt.setTime( res );
-        return formatter.format( dt );
+        return outFormatter.format( dt );
     }
-
 }
