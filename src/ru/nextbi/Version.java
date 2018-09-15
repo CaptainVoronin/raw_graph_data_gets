2aac7ee16788e23585b4bb3223c4a94315898fa4
CaptainVoronin <CaptainVoronin@gmail.com>
2018-09-15 14:25:14 +0300

    Не закончено

--- src/ru/nextbi/Version.java
+++ src/ru/nextbi/Version.java
@@ -1,9 +1,9 @@
 package ru.nextbi;
 
 public class Version {
-    public static int BUILD_NUMBER = 1;
-    public static int MAJOR_VERSION = 0;
-    public static int MINOR_VERSION = 1;
+    public static final int BUILD_NUMBER = 11;
+    public static final int MAJOR_VERSION = 0;
+    public static final int MINOR_VERSION = 1;
 
     public static final String getVersion()
     {
