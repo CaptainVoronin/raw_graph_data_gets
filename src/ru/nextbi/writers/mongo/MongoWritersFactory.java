8ccc0e437f5b2fa7fba8b710f2b7caa83afda27e
CaptainVoronin <CaptainVoronin@gmail.com>
2019-03-19 23:01:34 +0300

    Дописана MongoFactory

--- src/ru/nextbi/writers/mongo/MongoWritersFactory.java
+++ src/ru/nextbi/writers/mongo/MongoWritersFactory.java
@@ -1,6 +1,8 @@
 package ru.nextbi.writers.mongo;
 
+import ru.nextbi.GTDGenerator;
 import ru.nextbi.model.GraphElementDescription;
+import ru.nextbi.model.VertexDescription;
 import ru.nextbi.writers.IEdgeWriter;
 import ru.nextbi.writers.ILinkWriter;
 import ru.nextbi.writers.IVertexWriter;
@@ -11,23 +13,29 @@ import java.util.Map;
 
 public class MongoWritersFactory implements IWriterFactory{
 
+    MongoConnection connection;
+
     public MongoWritersFactory( Map<String, String> params )
     {
-
+        connection = new MongoConnection( params.get(GTDGenerator.DBHOST),
+                                          Integer.parseInt( params.get( GTDGenerator.DBPORT) ),
+                                          params.get( GTDGenerator.DBNAME ) );
     }
 
     @Override
     public IVertexWriter createVertexWriter(GraphElementDescription vd) throws IOException{
-        return null;
+        return new TMongoVertexWriter(  connection, (VertexDescription) vd);
     }
 
     @Override
-    public IEdgeWriter createEdgeWriter(GraphElementDescription vd){
-        return null;
+    public IEdgeWriter createEdgeWriter(GraphElementDescription ed)
+    {
+        return new TMongoEdgeWriter( connection, (VertexDescription) ed);
     }
 
     @Override
-    public ILinkWriter createLinkWriter(String parentClassName, String className){
-        return null;
+    public ILinkWriter createLinkWriter(String parentClassName, String className)
+    {
+        return new MongoLinkWriter( connection, parentClassName, className );
     }
 }
